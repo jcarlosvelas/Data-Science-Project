b7b5190659c2362cd89cde1aced4286cdbd636e6
jcarlosvelas <69486815+jcarlosvelas@users.noreply.github.com>
2020-08-21 21:18:01 -0600

    Delete notebook.py

--- notebook.py
+++
@@ -1,6 +0,0 @@
-#Hello, this notebook will be used for the Data Science Capstone Project
-#We import Pandas and NumPy
-import pandas as pd
-import numpy as np
-#Print a statement
-print('Hello Capstone Project Course!')
